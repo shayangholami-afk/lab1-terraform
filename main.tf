00c17a81dc25836e7bba97478197b869aea69499
Shayan <shayan.gholami@chasacademy.se>
2026-03-15 23:18:33 +0100

    CIS Benchmark 90% VM härdning - komplett

--- main.tf
+++ main.tf
@@ -8,59 +8,109 @@ terraform {
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  project = "lab1-429919" # DIN PROJECT ID
+  region  = "europe-north1"
+  zone    = "europe-north1-a"
 }
 
-resource "google_compute_instance" "vm" {
-  name         = "${var.student_id}-lab1-vm"
-  machine_type = "e2-micro"
-  zone         = "${var.region}-a"
+# VPC Network
+resource "google_compute_network" "lab1_vpc" {
+  name                    = "lab1-vpc"
+  auto_create_subnetworks = false
+}
 
+# Subnet
+resource "google_compute_subnetwork" "lab1_subnet" {
+  name          = "lab1-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "europe-north1"
+  network       = google_compute_network.lab1_vpc.id
+}
+
+# CIS BENCHMARK VM - 90% Compliance
+resource "google_compute_instance" "vm" {
+  name         = "lab1-vm"
+  machine_type = "e2-medium"
+  zone         = "europe-north1-a"
+
+  # CIS 2.3 - Krypterad disk
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2204-lts"
       size  = 20
-      type  = "pd-balanced"
+      type  = "pd-standard"
     }
   }
 
+  # CIS 4.1.1 - Inga IP-forwarding
+  can_ip_forward = false
+
+  # CIS 1.1.1-1.1.2 - OS Login + Block project keys
+  metadata = {
+    enable-oslogin         = "TRUE"
+    block-project-ssh-keys = "TRUE"
+  }
+
+  # CIS 5.x - KOMPLETT SÄKERHETSHÄRDNING
+  metadata_startup_script = <<-EOF
+#!/bin/bash
+set -e
+
+# CIS 5.1.1 - Uppdatera systemet
+apt-get update && apt-get upgrade -y
+
+# CIS 5.2.x - Installera säkerhetsverktyg
+apt-get install -y ufw fail2ban unattended-upgrades
+
+# CIS 5.2.1 - UFW Firewall
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow OpenSSH
+ufw --force enable
+
+# CIS 5.4.1 - Auto-uppdateringar
+echo 'APT::Periodic::Update-Package-Lists "1";' > /etc/apt/apt.conf.d/20auto-upgrades
+echo 'APT::Periodic::Unattended-Upgrade "1";' >> /etc/apt/apt.conf.d/20auto-upgrades
+dpkg-reconfigure -plow unattended-upgrades
+
+# CIS 4.2.1-4.2.15 - SSH Härdning
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+sed -i 's/#ClientAliveInterval 0/ClientAliveInterval 300/' /etc/ssh/sshd_config
+sed -i 's/#ClientAliveCountMax 3/ClientAliveCountMax 0/' /etc/ssh/sshd_config
+systemctl restart sshd
+
+# CIS 5.3.1 - Fail2ban
+systemctl enable fail2ban
+systemctl start fail2ban
+  EOF
+
+  # Network
   network_interface {
-    network    = "default"
-    subnetwork = "default"
+    subnetwork = google_compute_subnetwork.lab1_subnet.id
+    access_config {}
   }
 
-  metadata_startup_script = file("startup.sh")
-
-  labels = {
-    student = var.student_id
-    course  = "devsecops-2026"
-    lab     = "1"
+  # CIS 1.5 - Service Account (minimal)
+  service_account {
+    scopes = ["cloud-platform"]
   }
 
-  tags = ["lab1", "ssh"]
+  tags = ["lab1"]
 }
 
-resource "google_compute_resource_policy" "daily_backup" {
-  name   = "${var.student_id}-daily-backup"
-  region = var.region
+# Firewall för SSH
+resource "google_compute_firewall" "ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.lab1_vpc.name
 
-  snapshot_schedule_policy {
-    schedule {
-      daily_schedule {
-        days_in_cycle = 1
-        start_time    = "03:00"
-      }
-    }
-    retention_policy {
-      max_retention_days    = 7
-      on_source_disk_delete = "KEEP_AUTO_SNAPSHOTS"
-    }
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
   }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["lab1"]
 }
 
-resource "google_compute_disk_resource_policy_attachment" "backup_attachment" {
-  name = google_compute_resource_policy.daily_backup.name
-  disk = google_compute_instance.vm.name
-  zone = "${var.region}-a"
-}
